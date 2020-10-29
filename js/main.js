2ccc70894c3f2b63837bdc61469ca64985104a01
david cortesi <david.cortesi.097@gmail.com>
2020-10-29 16:29:51 +0100

    commit 2

--- js/main.js
+++ js/main.js
@@ -16,18 +16,37 @@ var alert = $(".alert");
 alert.text(randomArray);
 
 // ciclo timing TIMER di 30 secondi per la visione
-setTimeout( mostra , 3000 );
+setTimeout( mostra , 10000 );
 function mostra(){
   alert.hide();
 }
 //Array numeri inseriti
 var insertNumber = [];
 //prompt richiesta numeri
-setTimeout( richiestaF , 3500);
+setTimeout( richiestaF , 10500);
 function richiestaF(){
   for (var i = 0; i < 5 ; i++) {
     var richiesta = parseInt( prompt("inerisci i numeri che hai appena visto") );
     insertNumber.push(richiesta);
     }
+
 }
+
+//array numeri giusti
+setTimeout( punteggio , 30000)
+function punteggio(){
+  var rightAnswer = [];
+  var a = 0;
+  for (var i = 0; i < randomArray.length; i++) {
+    if ( insertNumber[a] == randomArray[i]) {
+      rightAnswer.push( insertNumber[a] );
+      a += 1;
+    } else {
+      a += 1;
+      }
+    }
+  $(".punteggio").text("COMPLIMENTI HAI INDOVINATO  " + rightAnswer.length + "  PAROLE SU 5")
+}
+
+console.log(randomArray);
 console.log(insertNumber);
